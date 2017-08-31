6f1cfa4f561e080e143f3e772a5e8f7322cf52d8
Luke Brady <lbrad23105@gmail.com>
2017-08-31 18:11:32 -0400

    Added support for both JSON and python dicts.

--- csv_pipeline.py
+++ csv_pipeline.py
@@ -2,21 +2,38 @@ import json
 import csv
 
 
+# Pipeline that takes an in-memory CSV object and converts it to Python Dictionary.
+# @param csv_obj: In-memory CSV file that will be converted to a Dictionary.
+# @param fieldnames: The keys used in the Dictionary document.
+# @returns json_array: Array of Dictionary objects that will entered into database.
+def csv_to_dict(csv_obj, fieldnames):
+    reader = csv.DictReader(csv_obj, fieldnames)
+    dict_array = []
+    for row in reader:
+        if test_dictionary(dict(row), fieldnames) is True:
+            dict_array.append(row)
+        else:
+            return
+    print(dict_array)
+    return dict_array
+
+
+# Pipeline that takes an in-memory CSV object and converts it to JSON.
 # Pipeline that takes an in-memory CSV object and converts it to JSON.
 # @param csv_obj: In-memory CSV file that will be converted to JSON.
 # @param fieldnames: The keys used in the JSON document.
 # @returns json_array: Array of JSON objects that will entered into database.
 def csv_to_json(csv_obj, fieldnames):
     reader = csv.DictReader(csv_obj, fieldnames)
-    dict_array = []
+    json_array = []
     for row in reader:
         if test_dictionary(dict(row), fieldnames) is True:
             dumped_row = json.dumps(row)
-            dict_array.append(row)
+            json_array.append(dumped_row)
         else:
             return
-    print(dict_array)
-    return dict_array
+    print(json_array)
+    return json_array
 
 
 # Helper function that will test if all fields are filled out.
@@ -33,7 +50,9 @@ def test_dictionary(dictionary, fieldnames):
 
 def main():
     c = open("C:\\Users\\lbrad23105\\Desktop\\test.csv", "r")
-    csv_to_json(c, ("Width","Height","Length","Area"))
+    d = open("C:\\Users\\lbrad23105\\Desktop\\test.csv", "r")
+    csv_to_dict(c, ("Width","Height","Length","Area"))
+    csv_to_json(d, ("Width", "Height", "Length", "Area"))
 
 main()
 
